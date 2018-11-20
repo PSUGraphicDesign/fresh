d2f44e6563a35dd7cc20b144388dd7f65908227b
Savannah Julian <savannah@oof.studio>
2018-11-20 10:09:24 -0800

    Adding class to menu links

--- app/site/snippets/blocks/menu.php
+++ app/site/snippets/blocks/menu.php
@@ -1,13 +1,13 @@
 <section class="menu">
   <ul class="menu__items">
     <li class="menu__item menu__item--attend">
-      <a href="<?= page($site->current_term())->ticket_link() ?>" target="_blank">Attend</a>
+      <a class="menu__item-link" href="<?= page($site->current_term())->ticket_link() ?>" target="_blank">Attend</a>
     </li>
     <li class="menu__item menu__item--home">
-      <a href="<?= url('/') ?>">Home</a>
+      <a class="menu__item-link" href="<?= url('/') ?>">Home</a>
     </li>
     <li class="menu__item menu__item--grads">
-      <a href="#grads">Grads</a>
+      <a class="menu__item-link" href="#grads">Grads</a>
     </li>
   </ul>
 </section>
