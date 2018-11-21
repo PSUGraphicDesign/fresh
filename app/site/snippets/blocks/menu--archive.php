ed47575d469cffa71bb8e7989452b31f5791f17c
Savannah Julian <savannah@oof.studio>
2018-11-20 19:51:44 -0800

    Archive menu styles

--- app/site/snippets/blocks/menu--archive.php
+++ app/site/snippets/blocks/menu--archive.php
@@ -1,7 +1,7 @@
 <section class="menu menu--archive">
   <ul class="menu__items">
     <li class="menu__item">
-      <a class="menu__item-link" href="#" target="_blank">Menu Item</a>
+      <a class="menu__item-link" href="<?= url('/') ?>" target="_blank">← View Current Term</a>
     </li>
   </ul>
 </section>
