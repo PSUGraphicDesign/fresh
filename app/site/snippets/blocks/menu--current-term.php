499e2e21e113060cc25a8af72fb971bacfa40e1a
Savannah Julian <savannah@oof.studio>
2018-11-24 21:03:17 -0800

    Adding specific classes to menu links

--- app/site/snippets/blocks/menu--current-term.php
+++ app/site/snippets/blocks/menu--current-term.php
@@ -1,10 +1,10 @@
 <section class="menu menu--current-term">
   <ul class="menu__items">
     <li class="menu__item menu__item--attend">
-      <a class="menu__item-link" href="<?= page($site->current_term())->ticket_link() ?>" target="_blank">Attend</a>
+      <a class="menu__item-link menu__item-link--attend" href="<?= page($site->current_term())->ticket_link() ?>" target="_blank">Attend</a>
     </li>
     <li class="menu__item menu__item--home">
-      <a class="menu__item-link" href="<?= url('/') ?>">
+      <a class="menu__item-link menu__item-link--home" href="<?= url('/') ?>">
         <? if ($page->termLogo()) { ?>
           <? snippet('blocks/responsive-image', ['image' => $page->termLogo()]) ?>
         <? } else { ?>
@@ -13,7 +13,7 @@
       </a>
     </li>
     <li class="menu__item menu__item--grads">
-      <a class="menu__item-link" href="#grads">Grads</a>
+      <a class="menu__item-link menu__item-link--grads" href="#grads">Grads</a>
     </li>
   </ul>
 </section>
