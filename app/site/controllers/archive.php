e4cc1b7de8ca7c9f5ec3da8248535447088cf2a8
August Miller <hello@gusmiller.com>
2024-01-04 11:01:42 -0800

    Avoid infinite redirect
    When the latest year has only one term and it's the current one, the archive enters an infinite loop.

--- app/site/controllers/archive.php
+++ app/site/controllers/archive.php
@@ -1,4 +1,10 @@
 <?php return function ($site, $pages, $page) {
-  # Just redirect to the most recent year:
-  go($page->years()->sortBy('title', 'desc')->first());
+  # Just redirect to the most recent year with a non-current term:
+  foreach ($page->years()->sortBy('title', 'desc') as $year) {
+    foreach ($year->terms() as $term) {
+      if (!$term->isCurrentTerm()) {
+        return go($term);
+      }
+    }
+  }
 };
